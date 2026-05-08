4b4fbf97b7ef9e97445a5e8823ac73e3708386c0
gaser014 <gaser.mohamed158@gmail.com>
2026-05-08 13:56:14 +0300

    dev updated_4

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,7 +1,10 @@
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:flowers_app/config/api/api_key.dart';
+import 'package:flowers_app/config/database/cache_helper.dart';
 import 'package:flowers_app/config/dependency_injection/di.dart';
+import 'package:flowers_app/core/data/data_sources/auth_local_data_source.dart';
 import 'package:flowers_app/core/routes/routes.dart';
 import 'package:flowers_app/features/login/presentation/view/pages/login_page.dart';
 import 'package:flowers_app/features/main/presentation/screens/main_view.dart';
@@ -259,6 +262,7 @@ abstract class AppRoutes {
       ),
       GoRoute(
         path: Routes.main,
+        name: Routes.main,
         pageBuilder: (context, state) => buildAnimatedPage(
           key: state.pageKey,
           child: const MainView(),
@@ -283,30 +287,30 @@ abstract class AppRoutes {
     redirect: (context, state) async {
       final currentLocation = state.matchedLocation;
 
-      if (currentLocation == Routes.splash) {
-        return null;
-      }
-
-      final token = await getIt<FlutterSecureStorage>().read(
-        key: APIkeys.accessToken,
-      );
-      final isLoggedIn = token != null && token.isNotEmpty;
       final authRoutes = [
-        // AuthRoutes.accountTypeSelection,
-        // AuthRoutes.phoneNumber,
+        Routes.login,
+        // Routes.register,
+        // Routes.forgetPassword,
+        // Routes.resetPassword,
         // AuthRoutes.otpVerification,
         // AuthRoutes.completeProfile,
         // AuthRoutes.success,
       ];
+      //
+      // if (!isLoggedIn && !authRoutes.contains(currentLocation)) {
+      //   // Redirect to account type selection (start of auth flow)
+      //   return Routes.login;
+      // }
 
-      if (!isLoggedIn && !authRoutes.contains(currentLocation)) {
-        // Redirect to account type selection (start of auth flow)
-        // return AuthRoutes.accountTypeSelection;
-      }
+      if (authRoutes.contains(currentLocation)) {
+        final token = await getIt<AuthLocalDataSourceContract>().getUserToken();
+        log('Auth Token: $token');
+        final isLoggedIn = token != null && token.isNotEmpty;
 
-      if (isLoggedIn && authRoutes.contains(currentLocation)) {
         // Redirect to home screen
-        // return Routes.home;
+        if (isLoggedIn) {
+          return Routes.main;
+        }
       }
 
       // No redirect needed
