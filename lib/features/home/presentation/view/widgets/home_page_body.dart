9dc7d226479ab150e9589c3f43f3cdd2be3e4e29
Ahmed Yosry <a7med.elesawy0155066@gmail.com>
2026-05-10 15:38:05 +0300

    feat: add view all navigation to categories section and register HomeCubit and AppSharedPreferences

--- lib/features/home/presentation/view/widgets/home_page_body.dart
+++ lib/features/home/presentation/view/widgets/home_page_body.dart
@@ -3,7 +3,10 @@ import 'package:flowers_app/features/home/presentation/view/widgets/best_seller_
 import 'package:flowers_app/features/home/presentation/view/widgets/category_list_section.dart';
 import 'package:flowers_app/features/home/presentation/view/widgets/occasion_section.dart';
 import 'package:flowers_app/features/home/presentation/view/widgets/section_header.dart';
+import 'package:flowers_app/features/main/presentation/view_model/cubit/home_cubit.dart';
+import 'package:flowers_app/features/main/presentation/view_model/cubit/home_events.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 
 class HomePageBody extends StatelessWidget {
@@ -11,19 +14,24 @@ class HomePageBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    final cubit = context.read<HomeCubit>();
+    return SafeArea(
       child: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SectionHeader(title: AppStrings.categories),
-            CategoryListSection(),
-            Gap(16),
-            SectionHeader(title: AppStrings.bestSeller),
-            BestSellerSection(),
-            Gap(16),
-            SectionHeader(title: AppStrings.occasion),
-            OccasionSection(),
+            SectionHeader(
+              title: AppStrings.categories,
+              onViewAllPressed: () =>
+                  cubit.doIndented(ChangeBottomNavIndexEvent(1)),
+            ),
+            const CategoryListSection(),
+            const Gap(16),
+            const SectionHeader(title: AppStrings.bestSeller),
+            const BestSellerSection(),
+            const Gap(16),
+            const SectionHeader(title: AppStrings.occasion),
+            const OccasionSection(),
           ],
         ),
       ),
